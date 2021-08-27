94c1dc1a98240bacaab0edbe2ae25d0f1cb21116
yb <123456@163.com>
2021-08-27 22:51:35 +0800

    first commit

---
+++ src/main/java/com/yb/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yb.git;
+
+/**
+ * @author yubin
+ * @create 2021-08-27 22:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}

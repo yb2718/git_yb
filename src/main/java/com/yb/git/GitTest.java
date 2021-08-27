46d16baab98eca67394286f9e16a24af42ef3c53
yb <123456@163.com>
2021-08-27 22:52:53 +0800

    second commit

--- src/main/java/com/yb/git/GitTest.java
+++ src/main/java/com/yb/git/GitTest.java
@@ -1,5 +1,6 @@
 package com.yb.git;
 
+
 /**
  * @author yubin
  * @create 2021-08-27 22:47
@@ -7,5 +8,6 @@ package com.yb.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello java");
+        System.out.println("hello scala");
     }
 }

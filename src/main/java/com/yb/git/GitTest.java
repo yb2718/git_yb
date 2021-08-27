0c4631a13226a6574a047eea4b5ca1b264c5bebf
yb <123456@163.com>
2021-08-27 23:06:37 +0800

    push commit

--- src/main/java/com/yb/git/GitTest.java
+++ src/main/java/com/yb/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello scala");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
